7f7bd7a242dd3fa714564348682296f26160c78a
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2021-04-02 23:43:49 +0800

    removed search button - changed to select country - added country api

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const search = document.querySelector("#search");
 
+        
 // FORMAT NUMBERS PUTTING A COMMA
 function formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
@@ -40,7 +41,6 @@ $(".search-icon").click(function () {
                 timeout: 3000,
             });
 
-            document.querySelector("#search").value = "";
 
         } else {
             let length = data.length;
@@ -58,7 +58,6 @@ $(".search-icon").click(function () {
             countryTitle.innerHTML = `<img src="https://www.countryflags.io/${countryCode}/flat/64.png"> `;
             countryTitle.append(data[index].Country + " COVID-19 Live Cases");
 
-            document.querySelector("#search").value = "";
         }
     })
 
@@ -69,24 +68,22 @@ $(".search-icon").click(function () {
             title: "SERVER ERROR",
             position: "topCenter",
             timeout: 3000,
-        });
-        document.querySelector("#search").value = "";
+        })
     })
 
 });
 
-fetch(`https://api.covid19api.com/summary`)
+    fetch(`https://api.covid19api.com/summary`)
     .then((res) => res.json())
     .then((data) => {
 
         if(data.Message != "") {
 
-            iziToast.information({
+            iziToast.error({
                 title: "Caching in progress",
                 position: "topCenter",
                 timeout: 3000,
             });
-            document.querySelector("#search").value = "";
         } else {
             const active = document.querySelector("#activeGlobal");
             const death = document.querySelector("#deathGlobal");
@@ -111,7 +108,6 @@ fetch(`https://api.covid19api.com/summary`)
                 position: "topCenter",
                 timeout: 3000,
             });
-            document.querySelector("#search").value = "";
         } else {
             let length = data.length;
             let index = length - 1;
@@ -126,7 +122,6 @@ fetch(`https://api.covid19api.com/summary`)
             recovered.append(formatNumber(data[index].Recovered));
             countryTitle.append(data[index].Country + " COVID-19 Live Cases");
 
-            document.querySelector("#search").value = "";
         }
     })
 
@@ -138,8 +133,84 @@ fetch(`https://api.covid19api.com/summary`)
             position: "topCenter",
             timeout: 3000,
         });
-        document.querySelector("#search").value = "";
     })
+
+
+const countriesList = document.getElementById("slct");
+let countries;
+
+countriesList.addEventListener("change", newCountrySelection => {
+    const selectedCountry = document.querySelector("#slct").value;
+    console.log(selectedCountry);
+
+    fetch(`https://api.covid19api.com/live/country/${selectedCountry}`)
+    .then((res) => res.json())
+    .then((data) => {
+
+        const active = document.querySelector("#active");
+        const death = document.querySelector("#death");
+        const recovered = document.querySelector("#recovered");
+        const countryTitle = document.querySelector("#country");
+
+        active.innerHTML = '';
+        death.innerHTML = '';
+        recovered.innerHTML = '';
+        countryTitle.innerHTML = '';
+
+        if(data.message == "Not Found") {
+
+            iziToast.error({
+                title: "Country not found",
+                position: "topCenter",
+                timeout: 3000,
+            });
+
+        } else {
+            let length = data.length;
+            let index = length - 1;
+
+            const active = document.querySelector("#active");
+            const death = document.querySelector("#death");
+            const recovered = document.querySelector("#recovered");
+            const countryTitle = document.querySelector("#country");
+            const countryCode = data[index].CountryCode;
+
+            active.append(formatNumber(data[index].Confirmed));
+            death.append(formatNumber(data[index].Deaths));
+            recovered.append(formatNumber(data[index].Recovered));
+            countryTitle.innerHTML = `<img src="https://www.countryflags.io/${countryCode}/flat/64.png"> `;
+            countryTitle.append(data[index].Country + " COVID-19 Live Cases");
+
+        }
+    })
+
+    .catch(err => {
+        console.log(err)
+
+        iziToast.error({
+            title: "SERVER ERROR",
+            position: "topCenter",
+            timeout: 3000,
+        })
+    })
+});
+
+
+// GET COUNTRY - REST COUNTRY API
+fetch("https://restcountries.eu/rest/v2/all")
+.then(res => res.json())
+.then(data => initialize(data))
+.catch(err => console.log("Error:", err));
+
+function initialize(countriesData) {
+  countries = countriesData;
+  let options = "";
+    
+  countries.forEach(country => options+=`<option value="${country.name}">${country.name}</option>`);
+  countriesList.innerHTML = options;
+  $("#slct option[value=Philippines]").prop("selected", true)
+
+}
 
 
 
